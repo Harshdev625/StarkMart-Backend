9026df54d91ec1d101e75e075d1994aaea105f8e
Harsh Dev <devh9933@gmail.com>
2023-10-15 21:57:51 +0530

    Changes After Deployment 5

--- index.js
+++ index.js
@@ -28,51 +28,60 @@ const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
 
 const endpointSecret = process.env.ENDPOINT_SECERT;
 
-server.post(
-  "/webhook",
-  express.raw({ type: "application/json" }),
-  async (request, response) => {
-    let event;
-    const signature = request.headers["stripe-signature"];
-    console.log("endpointSecret", endpointSecret, process.env.STRIPE_SERVER_KEY);
+server.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
 
-    try {
-      event = stripe.webhooks.constructEvent(
-        request.body,
-        signature,
-        endpointSecret
-      );
-    } catch (err) {
-      console.error(`Webhook Error: ${err.message}`);
-      response.status(400).send(`Webhook Error: ${err.message}`);
-      return;
-    }
+  let event;
 
-    console.log("Received signature:", signature);
-    console.log("Received request.body:", request.body);
-    console.log("Received endpointSecret:", endpointSecret);
-
-    // Handle the event
-    switch (event.type) {
-      case "payment_intent.succeeded":
-        const paymentIntentSucceeded = event.data.object;
-
-        const order = await Order.findById(
-          paymentIntentSucceeded.metadata.orderId
-        );
-        order.paymentStatus = "received";
-        await order.save();
-        console.log("Stripe Payment Working ", order.id);
-        break;
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-
-    // Return a 200 response to acknowledge receipt of the event
-    response.send();
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
   }
-);
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.amount_capturable_updated':
+      const paymentIntentAmountCapturableUpdated = event.data.object;
+      // Then define and call a function to handle the event payment_intent.amount_capturable_updated
+      break;
+    case 'payment_intent.canceled':
+      const paymentIntentCanceled = event.data.object;
+      // Then define and call a function to handle the event payment_intent.canceled
+      break;
+    case 'payment_intent.created':
+      const paymentIntentCreated = event.data.object;
+      // Then define and call a function to handle the event payment_intent.created
+      break;
+    case 'payment_intent.partially_funded':
+      const paymentIntentPartiallyFunded = event.data.object;
+      // Then define and call a function to handle the event payment_intent.partially_funded
+      break;
+    case 'payment_intent.payment_failed':
+      const paymentIntentPaymentFailed = event.data.object;
+      // Then define and call a function to handle the event payment_intent.payment_failed
+      break;
+    case 'payment_intent.processing':
+      const paymentIntentProcessing = event.data.object;
+      // Then define and call a function to handle the event payment_intent.processing
+      break;
+    case 'payment_intent.requires_action':
+      const paymentIntentRequiresAction = event.data.object;
+      // Then define and call a function to handle the event payment_intent.requires_action
+      break;
+    case 'payment_intent.succeeded':
+      const paymentIntentSucceeded = event.data.object;
+      // Then define and call a function to handle the event payment_intent.succeeded
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
 
 
 // JWT options
